6796e48bc8aa5bc0e0c34e518de0252dab75509b
KYLE S <79766794+418Gaming@users.noreply.github.com>
2021-12-09 11:18:17 -0500

    Add files via upload

--- config.lua
+++ config.lua
@@ -531,8 +531,8 @@ Config.Stores = {
 }
 
 Config.ClothingRooms = {
-    [1] = {requiredJob = "police", coords = vector3(454.43, -988.85, 30.69), cameraLocation = vector4(454.42, -990.52, 30.69, 358.48)},
-    [2] = {requiredJob = "ambulance", coords = vector3(318.15, -599.35, 43.29), cameraLocation = vector4(318.15, -599.35, 43.29, 332.09)}, -- Right double door in Hospital Map that is on QBCore GitHub
+    [1] = {requiredJob = "police", coords = vector3(462.42, -996.51, 30.69), cameraLocation = vector4(460.43, -996.66, 30.69, 270.32)},
+    [2] = {requiredJob = "ambulance", coords = vector3(305.74, -597.62, 43.28), cameraLocation = vector4(305.72, -597.52, 43.28, 157.13)}, -- Right double door in Hospital Map that is on QBCore GitHub
     [3] = {requiredJob = "police", coords = vector3(314.76, 671.78, 14.73), cameraLocation = vector4(317.62,  671.86,  14.73,  91.53)},
     [4] = {requiredJob = "ambulance", coords = vector3(338.70, 659.61, 14.71), cameraLocation = vector4(339.27,  661.63,  14.71,  315.5)},    
     [5] = {requiredJob = "ambulance", coords = vector3(-1098.45, 1751.71, 23.35), cameraLocation = vector4(-1097.15, 1750.19,  23.35, 38.70)},    
